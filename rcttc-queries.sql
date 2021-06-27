2b8b6deea877aa2ce9e3018d9416ef5712fc68e4
Marc Risdahl <MRisdahl@dev-10.com>
2021-06-26 19:08:36 -0500

    Completed Queries

--- rcttc-queries.sql
+++ rcttc-queries.sql
@@ -52,8 +52,69 @@ join performance p on p.performance_id = t.performance_id
 join theater th on th.theater_id = p.theater_id;
 
 -- Find customers without an address.
+select * from customer;
+select 
+first_name,
+last_name,
+address
+from customer
+where address = "";
+
 -- Recreate the spreadsheet data with a single query.
+select
+c.first_name,
+c.last_name,
+c.email,
+c.phone,
+c.address,
+t.seat,
+p.show_name,
+p.ticket_price,
+p.show_date,
+th.theater_name,
+th.address,
+th.phone,
+th.email
+from customer c
+join ticket t on t.customer_id = c.customer_id
+join performance p on p.performance_id = t.performance_id
+join theater th on th.theater_id = p.theater_id;
+
 -- Count total tickets purchased per customer.
+select
+c.first_name,
+c.last_name,
+count(t.ticket_id) number_of_tickets
+from customer c
+join ticket t on t.customer_id = c.customer_id
+group by c.first_name, c.last_name;
+
 -- Calculate the total revenue per show based on tickets sold.
+select
+p.show_name,
+sum(p.ticket_price) revenue
+from performance p
+join ticket t on t.performance_id = p.performance_id
+group by p.show_name;
+
 -- Calculate the total revenue per theater based on tickets sold.
+select * from performance;
+select
+th.theater_name,
+sum(p.ticket_price) revenue
+from theater th
+join performance p on p.theater_id = th.theater_id
+join ticket t on t.performance_id = p.performance_id
+group by th.theater_name;
+
 -- Who is the biggest supporter of RCTTC? Who spent the most in 2021?
+select
+c.first_name,
+c.last_name,
+sum(p.ticket_price) total_cost
+from customer c
+join ticket t on t.customer_id = c.customer_id
+join performance p on p.performance_id = t.performance_id
+group by c.first_name, c.last_name
+order by total_cost desc
+limit 1;
